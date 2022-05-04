5d7adb0ec3ef8aed4cf5640dadf8ae91c6d950a0
tam31 <98944772+tam31@users.noreply.github.com>
2022-05-04 09:22:05 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup (
+  include_package_data = True,
+  name='mycalc0021',
+  version='1.0.0',
+  description='oss-dev my calc ulator 0001',
+  author = 'teaminchoi'
+  author_email='wxogud3377@naver.com',
+  url='https://github.com/tam31/calc001/',
+  install_requires=['pytest'],
+  long_description_content_type='text/markdown',
+  classifiers=[
+    "Programming Language :: Python :: 3",
+    "Operationg System :: OS Independent",
+  ],
+)
